0cb08f875a318848d3151cc3cb1ed982ba392818
Dorian Marié <dorian@dorianmarie.com>
2024-08-23 21:51:11 +0200

    add json comparison and tests

--- spec/json_compare_spec.rb
+++ spec/json_compare_spec.rb
@@ -2,5 +2,14 @@
 
 require "spec_helper"
 
-RSpec.describe "each" do
+RSpec.describe "json-compare" do
+  it "works" do
+    expect(
+      `bin/json-compare package.json package.json 2>&1`
+    ).to eq("")
+
+    expect(
+      `bin/json-compare package.json package.json engines 2>&1`
+    ).to_not be_empty
+  end
 end
