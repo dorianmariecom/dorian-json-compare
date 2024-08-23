5b54a7da8a0ec9ae3b03a8b11e4c95ce76209889
Dorian Marié <dorian@dorianmarie.com>
2024-08-23 23:46:49 +0200

    update dorian-arguments to 0.4.0 and refactor json-compare script and spec

--- spec/json_compare_spec.rb
+++ spec/json_compare_spec.rb
@@ -4,9 +4,7 @@ require "spec_helper"
 
 RSpec.describe "json-compare" do
   it "works" do
-    expect(
-      `bin/json-compare package.json package.json 2>&1`
-    ).to eq("")
+    expect(`bin/json-compare package.json package.json 2>&1`).to eq("")
 
     expect(
       `bin/json-compare package.json package.json engines 2>&1`
